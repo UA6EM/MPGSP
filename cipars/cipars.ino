888f2294ed84bbc95334cdc9c040d0568841bfeb
Victor <ua6em@yandex.ru>
2024-03-23 18:55:32 +0300

    Update cipars.ino
    Оптимизирован для двух библиотек LCD, двух дисплеев и двух MCP резисторов

--- cipars/cipars.ino
+++ cipars/cipars.ino
@@ -1,30 +1,35 @@
 // Генератор для катушки Мишина на основе DDS AD9833
-
-/*  18.03.2024 - // https://codeload.github.com/UA6EM/Arduino-MCP4131/zip/refs/heads/mpgsp
-    17.03.2024 - // https://codeload.github.com/UA6EM/MCP4151/zip/refs/heads/mpgsp
-
-    16.03.2024 - регулировка MCP4131 продублирована на энкодер и кнопки
-    07.03.2024 - проба регулировки усиления на MCP4131
-    25.02.2024 - Версия CIPARS
-
-    06.05.2022
-    - Переработал программу для 2-строчного экрана
-
-   11.06.2022
-    - Во время работы отключил возможность крутить время
-    - В меня при работе изменил Таймре на Т, добавил знак V
-    - Добавил всем пинам имя
-    - Определил пины для потенциометра ...
-    - Добавил управление потенциометром с помощью энкодера
-
-   03.07.2022
-    - перенес инициализацию потенциометра в начало setup
-*/
+// Определения
+#define UA6EM                          // Замаркировать, если скетч для CIPARS
 #define SECONDS(x) ((x)*1000UL)
 #define MINUTES(x) (SECONDS(x) * 60UL)
 #define HOURS(x) (MINUTES(x) * 60UL)
 #define DAYS(x) (HOURS(x) * 24UL)
 #define WEEKS(x) (DAYS(x) * 7UL)
+#define ON_OFF_CASCADE_PIN 5           // Для выключения выходного каскада
+#define PIN_ENCODER1 6
+#define PIN_ENCODER2 7
+#define PIN_ENCODER3 3                 // Шимится, по прерыванию обрабатывается энкодер
+#define PIN_ENC_BUTTON 8
+#define PIN_ZUM 9
+#define CORRECT_PIN A7                 // Пин для внешней корректировки частоты.
+
+#define AD9833_CS   10
+#define AD9833_MOSI 2
+#define AD9833_MISO A1
+#define AD9833_SCK  A2
+
+#define  MCP41x1_SCK   13 // Define SCK pin for MCP4131 or MCP4151
+#define  MCP41x1_MOSI  11 // Define MOSI pin for MCP4131 or MCP4151
+#define  MCP41x1_MISO  12 // Define MISO pin for MCP4131 or MCP4151
+#define  MCP41x1_CS     4 // Define chipselect pin for MCP4131 or MCP4151
+/* 
+#define PPIN_CS  A0       //  Пины потенциометра
+#define PPIN_INC A1
+#define PPIN_UD  A2 */
+#define zFreq 2           // Делитель интервала - секунда/2
+
+// Глобальные переменные
 unsigned long interval = MINUTES(1);
 unsigned long oneMinute = MINUTES(1);
 unsigned long timers = MINUTES(5);  // время таймера 15, 30, 45 или 60 минут
@@ -38,82 +43,57 @@ unsigned long mill;  // переменная под millis()
 unsigned long prevCorrectTime = 0;
 unsigned long prevReadAnalogTime = 0;  // для отсчета 10 секунд между подстройкой частоты
 unsigned long prevUpdateDataIna = 0;   // для перерыва между обновлениями данных ina
+unsigned int  wiperValue;              // variable to hold wipervalue for MCP4131 or MCP4151
+unsigned int Data_ina219 = 0;
+long FREQ_MIN = 200000;                // 200kHz
+long FREQ_MAX = 500000;                // 500kHz
+long ifreq = FREQ_MIN;
+long freq = FREQ_MIN;
+const unsigned long freqSPI = 250000;  // Частота для SW SPI 
+const unsigned long availableTimers[] = { oneMinute * 15, oneMinute * 30, oneMinute * 45, oneMinute * 60 };
+const byte maxTimers = 4;
+int timerPosition = 0;
+volatile int newEncoderPos;            // Новая позиция энкодера
+static int currentEncoderPos = 0;      // Текущая позиция энкодера
 
-/*
-#include <LiquidCrystal_I2C_Menu.h>       // https://github.com/VladimirTsibrov/LiquidCrystal_I2C_Menu
-LiquidCrystal_I2C_Menu  lcd(0x3F, 16, 2); // https://tsibrov.blogspot.com/2020/09/LiquidCrystal-I2C-Menu.html
-*/
-
-#include <LCD_1602_RUS.h>         // https://github.com/ssilver2007/LCD_1602_RUS
-LCD_1602_RUS lcd(0x3F, 16, 2);  // используемый дисплей (0x3F, 16, 2) адрес,символов в строке,строк.
-//LCD_1602_RUS lcd(0x3F, 20, 4);
-
+#ifndef UA6EM
+#define I2C_ADDR 0x27
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(I2C_ADDR, 20, 4);
+#else
+#define I2C_ADDR 0x3F
+#include <LCD_1602_RUS.h> 
+LCD_1602_RUS lcd(I2C_ADDR, 16, 2);
+#endif
 
 #include <Wire.h>
 #include <SPI.h>
-
-unsigned int wiperValue;  // variable to hold wipervalue for MCP4131 or MCP4151
-#define MCP4151MOD        // снять ремарки если используем библиотеку MCP4151(SPI у неё 16 битный)
+#define MCP4151MOD  // снять ремарки если используем библиотеку MCP4151(SPI у неё 16 битный)
 #ifndef MCP4151MOD
-#include <MCP4131.h>      // https://github.com/UA6EM/Arduino-MCP4131/tree/mpgsp
-#define  CS_MCP 4         // Define chipselect pin for MCP4131
-MCP4131 Potentiometer(CS);
+#include <MCP4131.h>  // https://github.com/UA6EM/Arduino-MCP4131/tree/mpgsp
+MCP4131 Potentiometer(MCP41x1_CS);
 #else
-#include <MCP4151.h>      // https://github.com/UA6EM/MCP4151/tree/mpgsp
-#define  SCK   13
-#define  MOSI  11
-#define  MISO  12
-#define  CS_MCP 4         // Define chipselect pin for MCP4151
-MCP4151 Potentiometer(CS_MCP, MOSI, MISO, SCK, 4000000, 250000, SPI_MODE0);
+#include <MCP4151.h>  // https://github.com/UA6EM/MCP4151/tree/mpgsp
+MCP4151 Potentiometer(MCP41x1_CS, MCP41x1_MOSI, MCP41x1_MISO, MCP41x1_SCK, 4000000L, 250000L, SPI_MODE0);
 #endif
 
 #include "INA219.h"
 INA219 ina219;
 
-#define ON_OFF_CASCADE_PIN 5  // для выключения выходного каскада
-#define PIN_ENCODER1 6
-#define PIN_ENCODER2 7
-#define PIN_ENCODER3 3        // ШИМим пин, по прерыванию обрабатываем энкодер 
-#define PIN_ENCODER_BUTTON 8
-#define PIN_ZUM 9
-#define FNC_PIN 10            // Define chipselect pin for AD9833
-
-// пины потенциометра
-#define PIN_CS  A0
-#define PIN_INC A1
-#define PIN_UD  A2
-#define CORRECT_PIN A7  // пин для внешней корректировки частоты.
-
-
-#define zFreq 2           // делитель интервала - секунда/2
-unsigned int Data_ina219 = 0;
-const float refFreq = 25000000.0;  // Частота кристалла на плате AD9833
-
-long FREQ_MIN = 200000;  // 200kHz
-long FREQ_MAX = 500000;  // 500kHz
-long ifreq = FREQ_MIN;
-long freq = FREQ_MIN;
-const unsigned long freqSPI = 250000;
-
-const unsigned long availableTimers[] = { oneMinute * 15, oneMinute * 30, oneMinute * 45, oneMinute * 60 };
-const byte maxTimers = 4;
-int timerPosition = 0;
 // по умолчанию 50% потенциометра
-int currentPotenciometrPercent = 50;
-
-
-#include <AD9833.h>  // Пробуем новую по ссылкам ниже
-// https://github.com/UA6EM/AD9833/tree/mpgsp
-// https://codeload.github.com/UA6EM/AD9833/zip/refs/heads/mpgsp
+#ifdef MCP4151MOD
+int currentPotenciometrPercent = 127;
+#else
+int currentPotenciometrPercent = 63;
+#endif
 
 //--------------- Create an AD9833 object ----------------
+#include <AD9833.h>  // Пробуем новую по ссылкам в README закладке
 //AD9833 AD(10, 11, 13);     // SW SPI over the HW SPI pins (UNO);
-AD9833 Ad9833(FNC_PIN);      // HW SPI Defaults to 25MHz internal reference frequency
+//AD9833 Ad9833(AD9833_CS);  // HW SPI Defaults to 25MHz internal reference frequency
+AD9833 Ad9833(AD9833_CS, AD9833_MOSI, AD9833_SCK); // SW SPI
 
-
-
-/********* используемые подпрограммы выносим сюда *********/
-
+//    *** Используемые подпрограммы выносим сюда ***   //
 /*** Обработчик кнопки энкодера ***/
 //------Cl_Btn----------------------
 enum { sbNONE = 0,
@@ -167,16 +147,13 @@ class Cl_Btn {
       return state;
     }
 };
-
-Cl_Btn Btn1(PIN_ENCODER_BUTTON);  //Экземпляр обработчика для кнопки энкодера
+Cl_Btn Btn1(PIN_ENC_BUTTON);  //Экземпляр обработчика для кнопки энкодера
 
 /******* Простой энкодер *******/
 #include <util/atomic.h>    // для атомарности чтения данных в прерываниях
 #include <RotaryEncoder.h>  // https://www.arduino.cc/reference/en/libraries/rotaryencoder/
 RotaryEncoder encoder(PIN_ENCODER1, PIN_ENCODER2);
 
-volatile int newEncoderPos;        // новая позиция энкодера
-static int currentEncoderPos = 0;  // текущая позиция энкодера
 /*** Обработчик прерывания для энкодера ***/
 ISR(PCINT2_vect) {
   encoder.tick();
@@ -203,40 +180,30 @@ void setTimer() {
 }
 
 void testMCP4151() {
-  //wiperValue = 1;
+#ifdef MCP4151MOD
+static  int d_resis = 255;
+#else
+static  int d_resis = 127;
+#endif
+
   Serial.println("START Test MCP4151");
-  for (int i = 0; i < 128; i++) {
+  for (int i = 0; i < d_resis; i++) {
     Potentiometer.writeValue(i);
     delay(100);
     Serial.print("MCP4151 = ");
     Serial.println(i);
   }
-
-  for (int j = 127; j >= 1; --j) {
+  for (int j = d_resis; j >= 1; --j) {
     Potentiometer.writeValue(j);
     delay(100);
     Serial.print("MCP4151 = ");
     Serial.println(j);
   }
-
   Serial.println("STOP Test MCP4151");
 }
 
-
-
 void resetPotenciometer() {
   // Понижаем сопротивление до 0%:
-  analogWrite(PIN_UD, 0);          // выбираем понижение
-  digitalWrite(PIN_CS, LOW);       // выбираем потенциометр X9C
-  for (int i = 0; i < 100; i++) {  // т.к. потенциометр имеет 100 доступных позиций
-    analogWrite(PIN_INC, 0);
-    delayMicroseconds(1);
-    analogWrite(PIN_INC, 255);
-    delayMicroseconds(1);
-  }
-  digitalWrite(PIN_CS, HIGH); /* запоминаем значение и выходим из режима настройки */
-
-  // MCP4131
   wiperValue = 1;
   Potentiometer.writeValue(wiperValue);  // Set MCP4151 to position 1
 }
@@ -244,19 +211,6 @@ void resetPotenciometer() {
 // Уровень percent - от 0 до 100% от максимума.
 void setResistance(int percent) {
   resetPotenciometer();
-
-  // Поднимаем сопротивление до нужного:
-  analogWrite(PIN_UD, 255);   // выбираем повышение
-  digitalWrite(PIN_CS, LOW);  // выбираем потенциометр X9C
-  for (int i = 0; i < percent; i++) {
-    analogWrite(PIN_INC, 0);
-    delayMicroseconds(1);
-    analogWrite(PIN_INC, 255);
-    delayMicroseconds(1);
-  }
-
-  digitalWrite(PIN_CS, HIGH); /* запоминаем значение и выходим из режима настройки */
-
   for (int i = 0; i < percent; i++) {
     wiperValue = i;
   }
@@ -283,7 +237,6 @@ void processPotenciometr() {
       wiperValue -= 1;
     }
   }
-
   setResistance(currentPotenciometrPercent);
   Potentiometer.writeValue(wiperValue);  // Set MCP4131 to mid position
   Serial.print("wiperValue = ");
@@ -293,10 +246,10 @@ void processPotenciometr() {
 /*** Обработчик энкодера через ШИМ ***/
 void startEncoder() {
   attachInterrupt(1, Encoder2, RISING);
-  analogWrite(PIN_ENCODER3, 0x80);  // установим на пине частоту 490 гц скважность 2
-}
-
-void Encoder2(void) {               // процедура вызываемая прерыванием
+  analogWrite(PIN_ENCODER3, 0x80);  // Установим на пине частоту 490 гц скважность 2
+ }
+ 
+void Encoder2(void) {               // Процедура вызываемая прерыванием (обрабатываем энкодер)
   encoder.tick();
 }
 
@@ -310,13 +263,15 @@ unsigned long setTimerLCD(unsigned long timlcd) {
     timlcd = oldmemTimers;
     isWorkStarted = 0;
     lcd.setCursor(0, 3);
-    lcd.print("     ЗАВЕРШЕНО!     ");
+#ifdef UA6EM
+    lcd.print("    СТОП!     ");
+#else
+     lcd.print("    STOP!     ");
+#endif
     digitalWrite(ON_OFF_CASCADE_PIN, LOW);
     start_Buzzer();
     delay(3000);
     stop_Buzzer();
-    //AD9833reset();
-    //Ad9833.reset();
   }
   return timlcd;
 }
@@ -330,9 +285,7 @@ void stop_Buzzer() {
   digitalWrite(PIN_ZUM, LOW);
 }
 
-
 // ******************* Обработка AD9833 ***********************
-
 void /*long*/ readAnalogAndSetFreqInSetup() {
   int maxValue = 0;
   long freqWithMaxI = FREQ_MIN;
@@ -402,12 +355,12 @@ void readAnalogAndSetFreqInLoop() {
       Ad9833.setFrequency((float)freq, 0);
       delay(10);
     }
-
     ifreq = freqWithMaxI;
     Ad9833.setFrequency((float)ifreq, 0);
     prevReadAnalogTime = millis();
   }
 }
+
 
 //************************** SETUP *************************/
 void setup() {
@@ -416,16 +369,6 @@ void setup() {
   lcd.backlight();
   delay(10);
 
-  // настройки потенциометра
-  // сначала настраиваем потенциометр
-  pinMode(PIN_CS, OUTPUT);
-  pinMode(PIN_INC, OUTPUT);
-  pinMode(PIN_UD, OUTPUT);
-  digitalWrite(PIN_CS, HIGH);  // X9C в режиме низкого потребления
-  analogWrite(PIN_INC, 255);
-  analogWrite(PIN_UD, 255);
-
-  delay(30);
   // сбрасываем потенциометр в 0%
   resetPotenciometer();
   // после сброса устанавливаем значение по умолчанию
@@ -486,28 +429,22 @@ void setup() {
 void loop() {
   mill = millis();
   Btn1.run();
-
   if (Btn1.read() == sbLong) {
     oldmemTimers = memTimers;
     timMillis = millis();
     isWorkStarted = 1;
   }
-
   if (mill - prevUpdateDataIna > 1000 * 2) {
     Data_ina219 = ina219.shuntCurrent() * 1000;
     prevUpdateDataIna = millis();
   }
-
   myDisplay();
-
   if (isWorkStarted == 1) {
     memTimers = setTimerLCD(memTimers);
   }
-
   ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
     newEncoderPos = encoder.getPosition();
   }
-
   // если значение экодера поменялось
   if (currentEncoderPos != newEncoderPos) {
     // если работа ещё не началась, то можем устанавливать время
