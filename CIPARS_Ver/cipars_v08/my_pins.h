f2fb9ffe5fb6638fcf12077ae7df145bdf88c46e
Victor <ua6em@yandex.ru>
2024-03-31 18:38:37 +0300

    Add files via upload

---
+++ CIPARS_Ver/cipars_v08/my_pins.h
@@ -0,0 +1,32 @@
+/*
+ * A0 -
+ * A1 -
+ * A2 -
+ * A3 -
+ * A4 - LCD1602
+ * A5 - LCD1602
+ * A6 -
+ * A7 - SENS_IMPLOSION
+ * D0 - RX
+ * D1 - TX
+ * D2 - AD9833 SW_MOSI
+ * D3 - AD9833 SW_SCLK
+ * D4 - AD9833 SW_CS (FSYNC)
+ * D5 - LT1206_SHUTDOWN
+ * D6 - ENC_DT
+ * D7 - ENC_CLK
+ * D8 - ENC_SW
+ * D9 - BUZZER
+ * D10 - MCP4151 CS
+ * D11 - MCP4151 MOSI
+ * D12 -
+ * D13 - MCP4151 SCLK
+ * 
+ * D14 -
+ * D15 -
+ * D16 -
+ * D17 -
+ * D18 -
+ * D19 -
+ * /
+ */
