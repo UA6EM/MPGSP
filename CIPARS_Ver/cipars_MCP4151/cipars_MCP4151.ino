50790901b0946cdf4bc3e07b62ebc40bf90e0099
UA6EM <ua6em@yandex.ru>
2024-04-05 10:17:58 +0300

    update

--- CIPARS_Ver/cipars_MCP4151/cipars_MCP4151.ino
+++ CIPARS_Ver/cipars_MCP4151/cipars_MCP4151.ino
@@ -4,7 +4,7 @@
 
 // Определения
 //#define DEBUG                          // Замаркировать если не нужны тесты 
-#define UA6EM                          // Замаркировать, если скетч для пользователя CIPARS
+#define LCD_RUS                          // Замаркировать, если скетч для LCD_RUS
 #define SECONDS(x) ((x)*1000UL)
 #define MINUTES(x) (SECONDS(x) * 60UL)
 #define HOURS(x) (MINUTES(x) * 60UL)
@@ -66,10 +66,11 @@ volatile int newEncoderPos;            // Новая позиция энкоде
 static int currentEncoderPos = 0;      // Текущая позиция энкодера
 volatile  int d_resis = 127;
 
-#ifndef UA6EM
-#define I2C_ADDR 0x27 //0x3F //0x27
+#ifndef LCD_RUS
+#define I2C_ADDR 0x3F //0x27
 #include <LiquidCrystal_I2C.h>
-LiquidCrystal_I2C lcd(I2C_ADDR, 20, 4);
+//LiquidCrystal_I2C lcd(I2C_ADDR, 20, 4);
+LiquidCrystal_I2C lcd(I2C_ADDR, 16, 2);
 #else
 #define I2C_ADDR 0x3F
 #include <LCD_1602_RUS.h>
@@ -275,7 +276,7 @@ unsigned long setTimerLCD(unsigned long timlcd) {
     timlcd = oldmemTimers;
     isWorkStarted = 0;
     lcd.setCursor(0, 3);
-#ifdef UA6EM
+#ifdef LCD_RUS
     lcd.print("    СТОП!     ");
 #else
     lcd.print("    STOP!     ");
@@ -378,20 +379,20 @@ void myDisplay() {
   // 1 строка
   lcd.setCursor(0, 0);
   if (!isWorkStarted) {
-#ifdef UA6EM
+#ifdef LCD_RUS
     lcd.print("Время-");
 #else
     lcd.print("Times-");
 #endif
     lcd.print(memTimers / 60000);
     if (memTimers / 60000 > 0) {
-#ifdef UA6EM
+#ifdef LCD_RUS
       lcd.print(" мин. ");
 #else
       lcd.print(" min. ");
 #endif
     } else {
-#ifdef UA6EM
+#ifdef LCD_RUS
       lcd.print("0 мин. ");
 #else
       lcd.print("0 min. ");
@@ -402,7 +403,7 @@ void myDisplay() {
     if (memTimers > 60000) {
       // если больше минуты, то показываем минуты
       lcd.print(memTimers / 1000 / 60);
-#ifdef UA6EM
+#ifdef LCD_RUS
       lcd.print("мин.");
 #else
       lcd.print("min.");
@@ -410,7 +411,7 @@ void myDisplay() {
     } else {
       // если меньше минуты, то показываем секунды
       lcd.print(memTimers / 1000);
-#ifdef UA6EM
+#ifdef LCD_RUS
       lcd.print("сек.");
 #else
       lcd.print("sek.");
@@ -448,12 +449,8 @@ void setup() {
   Serial.begin(115200);
   Serial.println("START");
 
-#ifdef MCP4151MOD
-  lcd.begin();  // Зависит от версии библиотеки
-  //   lcd.init();
-#else
-  lcd.init();   // https://www.arduino.cc/reference/en/libraries/liquidcrystal-i2c/
-#endif
+  lcd.begin();    // Зависит от версии библиотеки
+  //lcd.init();
 
   lcd.backlight();
   delay(1000);
