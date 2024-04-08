4541f8b47cb74c1360f22c7fbf25cda72213a0b8
UA6EM <ua6em@yandex.ru>
2024-04-08 17:28:26 +0300

    update

--- ESP32_MCP41010/config.h
+++ ESP32_MCP41010/config.h
@@ -1,1 +1,7 @@
 #pragma once
+#pragma once
+#include <Arduino.h>
+#include "config.h"
+
+const char * ssid = "OpenNet";             // Название WIFI сети
+const char * password = "12345678";        // Пароль от WIFI сети не менее 8-и символов
