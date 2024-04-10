d19d4f879dc7bf14c098653fc063504d9a343944
UA6EM <ua6em@yandex.ru>
2024-04-10 18:52:18 +0300

    update

--- ESP32_MCP41010/ESP32_MCP41010.ino
+++ ESP32_MCP41010/ESP32_MCP41010.ino
@@ -175,7 +175,6 @@ int currentPotenciometrPercent = 127;
 //AD9833 Ad9833(AD9833_CS);  // HW SPI Defaults to 25MHz internal reference frequency
 AD9833 Ad9833(AD9833_CS, AD9833_MOSI, AD9833_SCK); // SW SPI speed 250kHz
 
-
 /******* Простой энкодер *******/
 //instead of changing here, rather change numbers above
 AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
@@ -208,6 +207,124 @@ void rotary_loop() {
 void IRAM_ATTR readEncoderISR() {
   rotaryEncoder.readEncoder_ISR();
 }
+
+
+// TFT ILI-9341
+#define TFT_GREY 0x5AEB
+#include <TFT_eSPI.h> // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+
+float sx = 0, sy = 1, mx = 1, my = 0, hx = -1, hy = 0;    // Saved H, M, S x & y multipliers
+float sdeg = 0, mdeg = 0, hdeg = 0;
+uint16_t osx = 120, osy = 120, omx = 120, omy = 120, ohx = 120, ohy = 120; // Saved H, M, S x & y coords
+uint16_t x0 = 0, x1 = 0, yy0 = 0, yy1 = 0;
+uint32_t targetTime = 0;                    // for next 1 second timeout
+static uint8_t conv2d(const char* p); // Forward declaration needed for IDE 1.6.x
+uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3), ss = conv2d(__TIME__ + 6); // Get H, M, S from compile time
+bool initial = 1;
+
+void testTFT() {
+  tft.init();
+  tft.setRotation(0);
+  //tft.fillScreen(TFT_BLACK);
+  //tft.fillScreen(TFT_RED);
+  //tft.fillScreen(TFT_GREEN);
+  //tft.fillScreen(TFT_BLUE);
+  //tft.fillScreen(TFT_BLACK);
+  tft.fillScreen(TFT_GREY);
+  tft.setTextColor(TFT_WHITE, TFT_GREY);  // Adding a background colour erases previous text automatically
+  // Draw clock face
+  tft.fillCircle(120, 120, 118, TFT_GREEN);
+  tft.fillCircle(120, 120, 110, TFT_BLACK);
+  // Draw 12 lines
+  for (int i = 0; i < 360; i += 30) {
+    sx = cos((i - 90) * 0.0174532925);
+    sy = sin((i - 90) * 0.0174532925);
+    x0 = sx * 114 + 120;
+    yy0 = sy * 114 + 120;
+    x1 = sx * 100 + 120;
+    yy1 = sy * 100 + 120;
+    tft.drawLine(x0, yy0, x1, yy1, TFT_GREEN);
+  }
+  // Draw 60 dots
+  for (int i = 0; i < 360; i += 6) {
+    sx = cos((i - 90) * 0.0174532925);
+    sy = sin((i - 90) * 0.0174532925);
+    x0 = sx * 102 + 120;
+    yy0 = sy * 102 + 120;
+    // Draw minute markers
+    tft.drawPixel(x0, yy0, TFT_WHITE);
+    // Draw main quadrant dots
+    if (i == 0 || i == 180) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+    if (i == 90 || i == 270) tft.fillCircle(x0, yy0, 2, TFT_WHITE);
+  }
+  tft.fillCircle(120, 121, 3, TFT_WHITE);
+  // Draw text at position 120,260 using fonts 4
+  // Only font numbers 2,4,6,7 are valid. Font 6 only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : . - a p m
+  // Font 7 is a 7 segment font and only contains characters [space] 0 1 2 3 4 5 6 7 8 9 : .
+  tft.drawCentreString("Time flies", 120, 260, 4);
+  targetTime = millis() + 1000;
+  unsigned long ttt = millis();
+
+  while (millis() - ttt < 60000) {
+    if (targetTime < millis()) {
+      targetTime += 1000;
+      ss++;              // Advance second
+      if (ss == 60) {
+        ss = 0;
+        mm++;            // Advance minute
+        if (mm > 59) {
+          mm = 0;
+          hh++;          // Advance hour
+          if (hh > 23) {
+            hh = 0;
+          }
+        }
+      }
+
+      // Pre-compute hand degrees, x & y coords for a fast screen update
+      sdeg = ss * 6;                // 0-59 -> 0-354
+      mdeg = mm * 6 + sdeg * 0.01666667; // 0-59 -> 0-360 - includes seconds
+      hdeg = hh * 30 + mdeg * 0.0833333; // 0-11 -> 0-360 - includes minutes and seconds
+      hx = cos((hdeg - 90) * 0.0174532925);
+      hy = sin((hdeg - 90) * 0.0174532925);
+      mx = cos((mdeg - 90) * 0.0174532925);
+      my = sin((mdeg - 90) * 0.0174532925);
+      sx = cos((sdeg - 90) * 0.0174532925);
+      sy = sin((sdeg - 90) * 0.0174532925);
+
+      if (ss == 0 || initial) {
+        initial = 0;
+        // Erase hour and minute hand positions every minute
+        tft.drawLine(ohx, ohy, 120, 121, TFT_BLACK);
+        ohx = hx * 62 + 121;
+        ohy = hy * 62 + 121;
+        tft.drawLine(omx, omy, 120, 121, TFT_BLACK);
+        omx = mx * 84 + 120;
+        omy = my * 84 + 121;
+      }
+
+      // Redraw new hand positions, hour and minute hands not erased here to avoid flicker
+      tft.drawLine(osx, osy, 120, 121, TFT_BLACK);
+      osx = sx * 90 + 121;
+      osy = sy * 90 + 121;
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+      tft.drawLine(ohx, ohy, 120, 121, TFT_WHITE);
+      tft.drawLine(omx, omy, 120, 121, TFT_WHITE);
+      tft.drawLine(osx, osy, 120, 121, TFT_RED);
+
+      tft.fillCircle(120, 121, 3, TFT_RED);
+    }
+  }
+}
+
+static uint8_t conv2d(const char* p) {
+  uint8_t v = 0;
+  if ('0' <= *p && *p <= '9')
+    v = *p - '0';
+  return 10 * v + *++p - '0';
+}
+
 
 //    *** Используемые подпрограммы выносим сюда ***   //
 #define FORMAT_SPIFFS_IF_FAILED true
@@ -289,19 +406,16 @@ void readSqlite3() {
     return;
 
   yield();
- const  char * sss;
+  const  char * sss;
   rc = db_exec(db1, sss = "SELECT count(*) FROM frequency");
   //SELECT id, COUNT(*) FROM times WHERE status = TRUE GROUP BY id ORDER BY COUNT(*) DESC
- // int sss = count.rc;
-// int cnt = sqlite_num__rows(rc);
-  
+ 
   Serial.print("COUNT = ");
-  Serial.println(cnt); // не правильно
-  Serial.println();  
+  //Serial.println(cnt); // не правильно
   Serial.println();
-  
+
   rc = db_exec(db1, "SELECT * FROM frequency");
-    if (rc != SQLITE_OK) {
+  if (rc != SQLITE_OK) {
     sqlite3_close(db1);
     return;
   }
@@ -591,7 +705,7 @@ void readAnalogAndSetFreqInLoop() {
     prevReadAnalogTime = millis();
   }
   alc_resis = getALC(ifreq); // рассчитать значение усиления сигнала
-  setALC( alc_resis);        // выставить усиление 
+  setALC( alc_resis);        // выставить усиление
 }
 
 // *** Вывод на дисплей ***
@@ -698,7 +812,7 @@ void setup() {
   ina219.calibrate(0.100, 0.32, 16, 3.2);
 
   SPI.begin();    // Для библиотеки MCP4xxxx обязательно!!!
-  
+
   // This MUST be the first command after declaring the AD9833 object
   Ad9833.begin();              // The loaded defaults are 1000 Hz SINE_WAVE using REG0
   Ad9833.reset();              // Ресет после включения питания
@@ -725,6 +839,7 @@ void setup() {
   //currentEncoderPos = wiperValue;
   Potentiometer.writeValue(wiperValue);  // Set MCP4131 or MCP4151 to mid position
 
+  testTFT();
   // Читаем базу
   readSqlite3();
 
@@ -1044,11 +1159,11 @@ void readDamp(int pw) {
   G18 - MCP41x1_SCK  18 <-> MCP41010_SCLK          // Define SCK pin for MCP41010
   G19 - MCP41x1_MISO 19  X  NOT CONNECTED          // Define MISO pin for MCP4131 or MCP41010
   G20 -
-  G21 - SDA // LCD, INA219 
+  G21 - SDA // LCD, INA219
   G22 - SCK // LCD, INA219
   G23 - MCP41x1_MOSI 23 <-> MCP41010_SDATA          // Define MOSI pin for MCP4131 or MCP41010
   G24 -
-  G25 - 
+  G25 -
   G26 -
   G27 -
   G28 -
@@ -1056,7 +1171,7 @@ void readDamp(int pw) {
   G30 -
   G31 -
   G32 - ON_OFF_CASCADE_PIN        32 <-> LT1206_SHUTDOWN
-  G33 - PIN_ZUM                   33 <-> BUZZER 
+  G33 - PIN_ZUM                   33 <-> BUZZER
   G34 - ROTARY_ENCODER_A_PIN      34 <-> ENC_CLK
   G35 - ROTARY_ENCODER_B_PIN      35 <-> ENC_DT
   G36 - ROTARY_ENCODER_BUTTON_PIN 36 <-> ENC_SW
