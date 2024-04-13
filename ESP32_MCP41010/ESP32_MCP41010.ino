8c5d56d0b252cd58b082d9dbdd4122aef60c7ee4
Victor <ua6em@yandex.ru>
2024-04-13 09:10:37 +0300

    Update ESP32_MCP41010.ino

--- ESP32_MCP41010/ESP32_MCP41010.ino
+++ ESP32_MCP41010/ESP32_MCP41010.ino
@@ -35,16 +35,6 @@
     WiFi версии 2.0.0                                  -
 */
 
-//#include <stdio.h>
-#include <stdlib.h>
-#include <Arduino.h>
-
-int zepFreq[42];
-char buffers[33];
-int zap = 51;
-String queryOne = "Hello";
-itoa(zap,buffers,10);
-queryOne+= String(buffers);
 
 // Определения
 #define WIFI                             // Используем модуль вайфая
@@ -91,6 +81,7 @@ queryOne+= String(buffers);
 #include <SPI.h>
 #include <FS.h>
 #include "SPIFFS.h"
+#include "LittleFS.h"
 
 #include "AiEsp32RotaryEncoder.h"
 #include "Arduino.h"
@@ -342,14 +333,88 @@ static uint8_t conv2d(const char* p) {
 
 #define FORMAT_SPIFFS_IF_FAILED true
 
-const char *data = "Callback function called";
-static int callback(void *data, int argc, char **argv, char **azColName) {
-  int i;
-  Serial.printf("%s: ", (const char *)data);
-  for (i = 0; i < argc; i++) {
-    Serial.printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+int zepDbFreq[42];
+int zepDbExpo[42];
+int zepDbPause[42];
+
+char buffers[33];
+int zap = 51;
+String queryOne = "SELECT * FROM frequency WHERE id = ";
+String queryTwo = "SELECT * FROM expositions WHERE id = ";
+String queryTree = "SELECT * FROM pauses WHERE id = ";
+
+const char* DBName = "/spiffs/zepper.db";
+
+// https://purecodecpp.com/archives/1502
+struct myDB {
+  int id = 1;
+  char names[30] = "Abdominal inflammation";
+  int f1 = 2720;
+  int f2 = 2489;
+  int f3 = 2170;
+  int f4 = 2000;
+  int f5 = 1865;
+  int f6 = 1800;
+  int f7 = 1600;
+  int f8 = 1550;
+  int f9 = 880;
+  int f10 = 832;
+  int f11 = 802;
+  int f12 = 787;
+  int f13 = 776;
+  int f14 = 727;
+  int f15 = 660;
+  int f16 = 465;
+  int f17 = 450;
+  int f18 = 444;
+  int f19 = 440;
+  int f20 = 428;
+  int f21 = 380;
+  int f22 = 250;
+  int f23 = 146;
+  int f24 = 125;
+  int f25 = 95;
+  int f26 = 72;
+  int f27 = 20;
+  int f28 = 1;
+  int f29 = 0;
+  int f30 = 0;
+  int f31 = NULL;
+  int f32 = NULL;
+  int f33 = NULL;
+  int f34 = NULL;
+  int f35 = NULL;
+  int f36 = NULL;
+  int f37 = NULL;
+  int f38 = NULL;
+  int f39 = NULL;
+  int f40 = NULL;
+  int f41 = NULL;
+  int f42 = NULL;
+};
+
+// Универсальный callback
+// Вызывается столько раз, сколько строк вернул запрос. Т.е. один вызов на строку!
+// Если вернуть не 0, то обработка запроса прекратится немедленно
+// Параметры:
+//    tagregArray - принимающий массив. НЕ ПРОВЕРЯЕТСЯ, что его РАЗМЕР ДОСТАТОЧЕН
+//    totalColumns - количество колонок
+//    azColVals - массив размером totalColumns - значения колонок в текстовом виде (если значение NULL, то нулевой указатель)
+//    azColNames - массив размером totalColumns - имена колонок
+//
+// ВНИМАНИЕ:   значение поля f1 помещается а НУЛЕВОЙ элемент принимающего массива,
+//             f2 - в первый, f3 - во второй и т.п.!
+//
+static int callback(void* tagregArray, int totalColumns, char** azColVals, char** azColNames) {
+  int* target = reinterpret_cast<int*>(tagregArray);
+  for (int i = 0; i < totalColumns; i++) {
+    const char* colName = azColNames[i];
+    const char nameFirstChar = colName[0];
+    if (nameFirstChar == 'f') {
+      const int index = atoi(colName + 1) - 1;
+      target[index] = azColVals[i] ? atoi(azColVals[i]) : 0;
+    }
   }
-  Serial.printf("\n");
   return 0;
 }
 
@@ -363,9 +428,9 @@ int db_open(const char *filename, sqlite3 **db) {
   }
   return rc;
 }
-
-char *zErrMsg = 0;
-int db_exec(sqlite3 *db, const char *sql) {
+/*
+  char *zErrMsg = 0;
+  int db_exec(sqlite3 *db, const char *sql) {
   Serial.println(sql);
   long start = micros();
   int rc = sqlite3_exec(db, sql, callback, (void *)data, &zErrMsg);
@@ -378,29 +443,30 @@ int db_exec(sqlite3 *db, const char *sql) {
   Serial.print(F("Time taken:"));
   Serial.println(micros() - start);
   return rc;
-}
+  }
+*/
 
-void readSqlite3() {
-  sqlite3 *db1;
-  int rc;
+int readSQLite3() {
+  printf("Go on!!!\n\n");
+
   if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
     Serial.println("Failed to mount file system");
-    return;
+    return 0;
   }
 
   // list SPIFFS contents
   File root = SPIFFS.open("/");
   if (!root) {
     Serial.println("- failed to open directory");
-    return;
+    return 0;
   }
+
   if (!root.isDirectory()) {
     Serial.println(" - not a directory");
-    return;
+    return 0;
   }
-  yield();
+
   File file = root.openNextFile();
-  yield();
   while (file) {
     if (file.isDirectory()) {
       Serial.print("  DIR : ");
@@ -413,28 +479,39 @@ void readSqlite3() {
     }
     file = root.openNextFile();
   }
-  yield();
+
+  sqlite3* db;
+  char* zErrMsg = 0;
   sqlite3_initialize();
-  if (db_open("/spiffs/zepper.db", &db1))
-    return;
 
-  yield();
-  const  char * sss;
-  rc = db_exec(db1, sss = "SELECT count(*) FROM frequency");
-  //SELECT id, COUNT(*) FROM times WHERE status = TRUE GROUP BY id ORDER BY COUNT(*) DESC
- 
-  Serial.print("COUNT = ");
-  //Serial.println(cnt); // не правильно
-  Serial.println();
-
-  rc = db_exec(db1, "SELECT * FROM frequency");
-  if (rc != SQLITE_OK) {
-    sqlite3_close(db1);
-    return;
+  int rc = sqlite3_open(DBName /*"/spiffs/zepper.db"*/, &db);
+  if (rc) {
+    printf("Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return (1);
   }
-  yield();
-  sqlite3_close(db1);
+
+  // Принимающий массив передаётся чертвёртым параметром!
+  itoa(zap, buffers, 10);
+  queryOne += String(buffers);
+
+  rc = sqlite3_exec(db, queryOne.c_str(), callback, zepDbFreq, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbFreq) / sizeof(zepDbFreq[0]); i++) {
+      printf("zepDbFreq[%d]=%d\n", i, zepDbFreq[i]);
+    }
+  }
+  Serial.println(queryOne);
+
+  sqlite3_close(db);
+  return 0;
 }
+
 // ************** END SQLITE3 *************** //
 
 
@@ -800,7 +877,7 @@ void setup() {
   lcd.init();     //
 
   lcd.backlight();
-  delay(1000);
+  delay(100);
 
   // сбрасываем потенциометр в 0%
   resetPotenciometer();
@@ -808,7 +885,7 @@ void setup() {
   setResistance(currentPotenciometrPercent);
 
   // ждем секунду после настройки потенциометра
-  delay(1000);
+  delay(100);
 
   Btn1.init();
 
@@ -838,8 +915,9 @@ void setup() {
   Ad9833.setFrequency((float)FREQ_MIN, AD9833_SINE);
 
   Serial.print("freq=");
-  Serial.println(FREQ_MIN);
-
+  Serial.print(FREQ_MIN);
+  Serial.println(" Hz");
+  
   Data_ina219 = ina219.shuntCurrent() * 1000;
   myDisplay();
   delay(100);
@@ -855,12 +933,14 @@ void setup() {
   testTFT();
   // Читаем базу
   readSQLite3();
-  for(int i = 0; i < 42; i++){
+  for (int i = 0; i < 42; i++) {
     Serial.print("f");
-    Serial.print(i+1);
+    Serial.print(i + 1);
     Serial.print(" = ");
-    Serial.println(zepFreq[i]);
+    Serial.println(zepDbFreq[i]);
   }
+
+  Serial.println(queryOne);
 
 }  //******** END SETUP ********//
 
