5c1f54fe8fc545b59557e136cbe319a13176b00d
UA6EM <ua6em@yandex.ru>
2024-04-19 09:32:40 +0300

    update

--- ESP32_MCP41010/ESP32_MCP41010.ino
+++ ESP32_MCP41010/ESP32_MCP41010.ino
@@ -813,6 +813,174 @@ void myDisplay() {
 }
 
 
+int readSQLite3() {
+  printf("Go on!!!\n\n");
+
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    Serial.println("Failed to mount file system");
+    return 0;
+  }
+
+  // list SPIFFS contents
+  File root = SPIFFS.open("/");
+  if (!root) {
+    Serial.println("- failed to open directory");
+    return 0;
+  }
+
+  if (!root.isDirectory()) {
+    Serial.println(" - not a directory");
+    return 0;
+  }
+
+  File file = root.openNextFile();
+  while (file) {
+    if (file.isDirectory()) {
+      Serial.print("  DIR : ");
+      Serial.println(file.name());
+    } else {
+      Serial.print("  FILE: ");
+      Serial.print(file.name());
+      Serial.print("\tSIZE: ");
+      Serial.println(file.size());
+    }
+    file = root.openNextFile();
+  }
+
+  sqlite3* db;
+  sqlite3_initialize();
+  int rc = sqlite3_open(DBName /*"/spiffs/zepper.db"*/, &db);
+  if (rc) {
+    printf("Can't open database: %s\n", sqlite3_errmsg(db));
+    sqlite3_close(db);
+    return (1);
+  }
+
+  // Принимающий массив передаётся чертвёртым параметром!
+  // Формируем строки запросов к таблицам базы данных
+  itoa(numerInTable, buffers, 10);
+  queryFreq  += String(buffers);
+  queryExpo  += String(buffers);
+  queryPause += String(buffers);
+  queryGen   += String(buffers);
+  querySig   += String(buffers);
+
+  Serial.println();
+  Serial.print("миллис начала = ");
+  unsigned long gomill = millis();
+  Serial.println(gomill);
+  Serial.println();
+
+  //заполним массив частот
+  rc = sqlite3_exec(db, queryFreq.c_str(), callback, zepDbFreq, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbFreq) / sizeof(zepDbFreq[0]); i++) {
+      printf("zepDbFreq[%d]=%d\n", i, zepDbFreq[i]);
+    }
+  }
+  Serial.print(queryFreq);
+  Serial.println("  - Обработан");
+
+  //заполним массив экспозиций
+  rc = sqlite3_exec(db, queryExpo.c_str(), callback, zepDbExpo, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbExpo) / sizeof(zepDbExpo[0]); i++) {
+      printf("zepDbExpo[%d]=%d\n", i, zepDbExpo[i]);
+    }
+  }
+  Serial.print(queryExpo);
+  Serial.println("  - Обработан");
+
+  //заполним массив пауз
+  rc = sqlite3_exec(db, queryPause.c_str(), callback, zepDbPause, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbPause) / sizeof(zepDbPause[0]); i++) {
+      printf("zepDbPause[%d]=%d\n", i, zepDbPause[i]);
+    }
+  }
+  Serial.print(queryPause);
+  Serial.println("  - Обработан");
+
+  //заполним массив режимов генератора
+  rc = sqlite3_exec(db, queryGen.c_str(), callback, zepDbModeGen, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbModeGen) / sizeof(zepDbModeGen[0]); i++) {
+      printf("zepDbModeGen[%d]=%d\n", i, zepDbModeGen[i]);
+    }
+  }
+  Serial.print(queryGen);
+  Serial.println("  - Обработан");
+
+  //заполним массив режимов вида сигнала генератора
+  rc = sqlite3_exec(db, querySig.c_str(), callback, zepDbModeSig, &zErrMsg);
+  if (rc != SQLITE_OK) {
+    fprintf(stderr, "SQL error: %s\n", zErrMsg);
+    sqlite3_free(zErrMsg);
+  }
+  else {
+    // ПЕЧАТЬ массива для проверки
+    for (int i = 0; i < sizeof(zepDbModeSig) / sizeof(zepDbModeSig[0]); i++) {
+      printf("zepDbModeSig[%d]=%d\n", i, zepDbModeSig[i]);
+    }
+  }
+  Serial.print(querySig);
+  Serial.println("  - Обработан");
+
+  Serial.println();
+  Serial.print("миллис окончания = ");
+  Serial.println(millis());
+  Serial.print("Время обработки базы = ");
+  Serial.print((float)(millis() - gomill) / 1000, 3);
+  Serial.println(" сек");
+  Serial.println();
+
+  sqlite3_close(db);
+  setStructure(0);
+  printStruct();
+
+  return 0;
+}    // ************** END SQLITE3 *************** //
+
+void testMCP41010() {
+  d_resis = 255;
+
+  Serial.println("START Test MCP41010");
+  for (int i = 0; i < d_resis; i++) {
+    Potentiometer.writeValue(i);
+    delay(100);
+    Serial.print("MCP41010 = ");
+    Serial.println(i);
+  }
+  for (int j = d_resis; j >= 1; --j) {
+    Potentiometer.writeValue(j);
+    delay(100);
+    Serial.print("MCP41010 = ");
+    Serial.println(j);
+  }
+  Serial.println("STOP Test MCP41010");
+}
+
+
 //************************** SETUP *************************/
 void setup() {
   Serial.begin(115200);
@@ -841,7 +1009,8 @@ void setup() {
   digitalWrite(PIN_ZUM, LOW);
   digitalWrite(PIN_RELE, LOW);
   digitalWrite(ON_OFF_CASCADE_PIN, HIGH);
-
+  pinMode(ROTARY_ENCODER_A_PIN, INPUT_PULLUP);
+  pinMode(ROTARY_ENCODER_B_PIN, INPUT_PULLUP);
 
   ina219.begin(0x40);                 // (44) i2c address 64=0x40 68=0х44 исправлять и в ina219.h одновременно
   ina219.configure(0, 2, 12, 12, 7);  // 16S -8.51ms
@@ -884,7 +1053,6 @@ void setup() {
 
   // Читаем базу
   readSQLite3();
-
 }  //******** END SETUP ********//
 
 
@@ -960,7 +1128,7 @@ void goZepper() {
   bool fgen = false; // синус / меандр = true (отслеживание смены режима)
 
   do {
-#ifdef DEBUG    
+#ifdef DEBUG
     printStruct();
 #endif
 
@@ -1072,4 +1240,24 @@ void setStructure(int strc) {
   Cicle.Freq     = zepDbFreq[strc];
   Cicle.Exposite = zepDbExpo[strc];
   Cicle.Pause    = zepDbPause[strc];
+}
+
+void readDamp(int pw) {
+#ifdef DEBUG
+  Serial.print("Разрешение выхода = ");
+  bool on_off = digitalRead(ON_OFF_CASCADE_PIN);
+  if (on_off) { Serial.println("Включено");}
+    else {Serial.println("Выключено");}
+ 
+  Serial.print("Выход сигнала на разъём ");
+  if (digitalRead(PIN_RELE)) {
+    Serial.println("ZEPPER");
+    Serial.print("U = ");
+    Serial.println(pw);
+  } else {
+    Serial.println("STATIC");
+    Serial.print("Мощность выхода = ");
+    Serial.println(currentEncoderPos);
+  }
+#endif
 }
